91109227a64380aebf03f5bf2b88ad7252159c24
rheehose2 <sea35652@gmail.com>
2026-02-03 23:30:35 +0900

    20251223 버전 실제 소스 통합 및 구조 정리 완료

---
+++ public/20251223/header.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<header style="background-color: lightblue; ">
+	<h1 align="center">쇼핑몰 회원관리 ver1.0</h1>
+	<h3 style="background-color:  gray; ">&emsp;&emsp;
+		<a href="join.jsp">회원등록</a> &emsp;&emsp;
+		<a href="list.jsp">회원목록조회/수정</a> &emsp;&emsp;
+		<a href="slaes.jsp">회원매출조회</a> &emsp;&emsp;
+		<a href="index.jsp">홈으로.</a>
+	</h3>
+
+</header>
+
+</body>
+</html>
---
+++ public/20251223/sql.sql
@@ -0,0 +1,33 @@
+CREATE TABLE member_tbl_02(
+	custno number(6) not null,
+	custname varchar2(20),
+	phone varchar2(13),
+	address varchar2(60),
+	joindate date,
+	grade char(1),
+	city char(2),
+	primary key(custno)
+);
+
+INSERT into member_tbl_02 values(100001,'김행복','010-1111-2222','서울 동대문구 휘경1동','20151202','A','01');
+INSERT into member_tbl_02 values(100002,'이축복','010-1111-3333','서울 동대문구 휘경2동','20151206','B','01');
+INSERT into member_tbl_02 values(100003,'장믿음','010-1111-4444','울릉군 울릉읍 독도1리','20151001','B','30');
+INSERT into member_tbl_02 values(100004,'최사랑','010-1111-5555','울릉군 울릉읍 독도2리','20151113','A','30');
+INSERT into member_tbl_02 values(100005,'진평화','010-1111-6666','제주도 제주시 외나무골','20151235','B','60');
+INSERT into member_tbl_02 values(100006,'차공단','010-1111-7777','제주도 제주시 감나무골','20151211','C','90');
+
+select * from member_tbl_02;
+
+CREATE TABLE money_tbl_02(
+	custno number(6) not null,
+	salenol number(8) not null,
+	pcost number(8),
+	amount number(4),
+	price number(8),
+	pcode varchar2(4),
+	sdate date,
+	primary key(custno, salenol)
+);
+
+
+insert into money_tbl_02 values(100001,20160001,500,5,2500,'A001','20160101');
